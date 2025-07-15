68cf31d2b471a6195f9483a396fa357f8f2d0041
David Williams <davidw1457@gmail.com>
2025-07-14 22:14:46 -0500

    ch5.l3

--- src/main.py
+++ src/main.py
@@ -4,6 +4,11 @@ from gencontent import generate_page
 def main():
     copy_files_recursive("static", "public")
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
+    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
+    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
+    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+
 
 if __name__ == "__main__":
     main()
