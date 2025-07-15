2055230901fa3ba8fb083f78d5a53ca34c157b84
David Williams <davidw1457@gmail.com>
2025-07-14 23:10:00 -0500

    fix docs

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     basepath = "/"
     if len(sys.argv) > 1:
         basepath = sys.argv[1]
-    copy_files_recursive("static", "public")
+    copy_files_recursive("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
